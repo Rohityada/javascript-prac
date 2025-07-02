c87292ea1db8aa7945ed3c1671fca20fa90392a0
Alpha23 <35468091+Rohityada@users.noreply.github.com>
2025-07-02 10:55:32 +0000

    03_comparision

---
+++ 01_basics/03_comparision.js
@@ -0,0 +1,34 @@
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) % 4);
+ 
+console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);
+
+console.log("2"> 1);
+console.log(2 >= 1);
+console.log(2 > 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
